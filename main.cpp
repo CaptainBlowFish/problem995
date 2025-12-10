7f4e721e7d1a7fff323f02d7ddf3ab288e909f15
CaptainBlowFish <71935477+CaptainBlowFish@users.noreply.github.com>
2025-12-10 11:28:31 -0500

    added basic functionality. is able to build a vector with all numbers in the sequence up to the nth triangle number.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,57 @@
+// Problem 955 
+// Project Euler
+// Jacob Milham
+
 #include <iostream>
 #include <vector>
 #include <string>
+#include <vector>
 
 using namespace std;
 
+bool is_triangle_number(unsigned long);
+void add_next_number(vector<unsigned long> *,bool,unsigned long);
+
+
 int main()
 {
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+	vector<unsigned long> *numbers = new vector<unsigned long>(1,3);
+	int triangleCount = 0;
+	unsigned long currentNumber = 3;
+	unsigned long count = 1;
+	while (triangleCount<4) {
+		currentNumber = numbers->back();
+		cout << currentNumber << "\n";
+		if (is_triangle_number(currentNumber)) {
+			add_next_number(numbers,true,count);
+			triangleCount++;
+		} else {
+			add_next_number(numbers,false,count);
+		}
+		count++;
+	}
+	
+	return EXIT_SUCCESS;
+}
 
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl;
+bool is_triangle_number(unsigned long number) {
+	bool found = false;
+	for(unsigned long i=0;i<number;i++) {
+		if (number == i*(i+1)/2) {
+			found = true;
+			break;
+		}
+	}
+	return found;
+}
+void add_next_number(vector<unsigned long> *numbers,bool triangle,unsigned long count) {
+	unsigned long current = numbers->back();
+	unsigned long last;
+	
+	if (triangle) {
+		numbers->push_back(current + 1);
+	} else {
+		last = numbers->at(count-2);
+		numbers->push_back(2*current - last + 1);
+	}
 }
