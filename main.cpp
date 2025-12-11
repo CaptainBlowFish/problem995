69f7ef446e2c64a8630e7f6eae373931738b20b7
CaptainBlowFish <71935477+CaptainBlowFish@users.noreply.github.com>
2025-12-11 11:12:40 -0500

    fixed cout stuff

--- main.cpp
+++ main.cpp
@@ -6,23 +6,25 @@
 
 using namespace std;
 
+const int NTH_TRIANGLE_NUMBER = 70;
+
 bool is_triangle_number(unsigned long,unsigned long &);
 void get_next_number(unsigned long &, unsigned long &, bool);
 
 int main()
 {
 	int triangleCount = 0;
-	unsigned long currentNumber = 3;
+	unsigned long currentNumber = 3; // this is the first triangle number used in the sequence
 	unsigned long lastNumber = 1; // fun fact 1 is the first triangle number
-	unsigned long count = 0;
-	unsigned long last_i_value = 0; //this is to skip some redundant loops in checking for triangle numbers.
+	unsigned long count = 0; // the number of items iterated over in the sequence
+	unsigned long last_i_value = 0; // this is to skip some redundant loops in checking for triangle numbers
 
-	while (triangleCount<70) {
+	while (triangleCount<NTH_TRIANGLE_NUMBER) {
 		
 		if (is_triangle_number(currentNumber,last_i_value)) {
+			cout << triangleCount << ":" << currentNumber << "\n";
 			get_next_number(currentNumber,lastNumber,true);
 			triangleCount++;
-			cout << triangleCount << ":" << currentNumber << "\n";
 
 		} else {
 			get_next_number(currentNumber,lastNumber,false);
